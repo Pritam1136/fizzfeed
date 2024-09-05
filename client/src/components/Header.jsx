b9a7d27ae3d008115a08d92e3bde7ea5d0b81767
Pritam Roy <pritamroy1136@gmail.com>
2024-09-05 12:27:47 +0530

    Updated

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import "./Header.css";
+
+function Header() {
+  return (
+    <div className="Header">
+      <div>GatherUp</div>
+      <button>Login</button>
+    </div>
+  );
+}
+
+export default Header;
--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,7 +1,55 @@
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+
 export const signup = async (req, res) => {
-  res.json({
-    data: "you hit the signup form.",
-  });
+  try {
+    const { fullname, username, email, password } = req.body;
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (!emailRegex.test(email)) {
+      return res.status(400).json({ error: "Invalid email format" });
+    }
+
+    const existingUser = await User.findOne({ username });
+
+    if (existingUser) {
+      return res.status(400).json({ error: "Username is already taken" });
+    }
+
+    const existingEmail = await User.findOne({ email });
+
+    if (existingEmail) {
+      return res.status(400).json({ error: "Email is already taken" });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      fullname: fullname,
+      username: username,
+      email: email,
+      password: hashedPassword,
+    });
+
+    if (newUser) {
+      generateTokenAndSetCookie(newUser._id, res);
+      await newUser.save();
+
+      res.status(201).json({
+        _id: newUser._id,
+        fullname: newUser.fullname,
+        username: newUser.username,
+        email: newUser.email,
+        followers: newUser.followers,
+        following: newUser.following,
+        profileImg: newUser.profileImg,
+        coverImg: newUser.coverImg,
+      });
+    } else {
+    }
+  } catch (error) {}
 };
 
 export const login = async (req, res) => {
